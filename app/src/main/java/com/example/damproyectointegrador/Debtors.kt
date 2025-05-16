bfc9d8a99410f10f93ce55f2c3fdb4ca78477c0d
Mirta Verón <mir.cv@hotmail.com>
2025-05-16 08:34:46 -0300

    Add intents to Debtors and MembershipCard

--- app/src/main/java/com/example/damproyectointegrador/Debtors.kt
+++ app/src/main/java/com/example/damproyectointegrador/Debtors.kt
@@ -1,12 +1,16 @@
 package com.example.damproyectointegrador
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class Debtors : AppCompatActivity() {
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +19,26 @@ class Debtors : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.item_home -> {
+                    val intent = Intent(this, MenuActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+
+                R.id.item_salir -> {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+
+                else -> false
+            }
         }
     }
 }
