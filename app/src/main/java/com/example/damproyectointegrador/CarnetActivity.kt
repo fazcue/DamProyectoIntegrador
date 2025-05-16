12bc648dabf1e108d4beb432617b5781f3420f75
Martin <martinc1998a@gmail.com>
2025-05-15 22:22:13 -0300

    adding intents to register and carnet activities

--- app/src/main/java/com/example/damproyectointegrador/CarnetActivity.kt
+++ app/src/main/java/com/example/damproyectointegrador/CarnetActivity.kt
@@ -1,12 +1,20 @@
 package com.example.damproyectointegrador
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class CarnetActivity : AppCompatActivity() {
+
+    private lateinit var btnBuscar: Button
+
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +23,31 @@ class CarnetActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        btnBuscar = findViewById(R.id.btn_buscar)
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        btnBuscar.setOnClickListener {
+            val intent = Intent(this, MembershipCard::class.java)
+            startActivity(intent)
+        }
+
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.item_home -> {
+                    val intent = Intent(this, MenuActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.item_salir -> {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                else -> false
+            }
         }
     }
 }
