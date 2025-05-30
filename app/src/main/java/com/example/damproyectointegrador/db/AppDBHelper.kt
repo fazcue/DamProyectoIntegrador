1921180802acf05015878e3bcfd5fb34737db86f
Facundo Azcue <fazcue@gmail.com>
2025-05-29 23:35:55 -0300

    Fix datatypes

--- app/src/main/java/com/example/damproyectointegrador/db/AppDBHelper.kt
+++ app/src/main/java/com/example/damproyectointegrador/db/AppDBHelper.kt
@@ -4,13 +4,13 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
-class AppDBHelper(context: Context) : SQLiteOpenHelper(context, "clubDeportivo7", null, 10) {
+class AppDBHelper(context: Context) : SQLiteOpenHelper(context, "clubDeportivo7", null, 11) {
     override fun onCreate(db: SQLiteDatabase) {
         db.execSQL("""
             CREATE TABLE IF NOT EXISTS users (
             	id INTEGER PRIMARY KEY AUTOINCREMENT,
-            	username VARCHAR(20) NOT NULL,
-            	password VARCHAR(15) NOT NULL,
+            	username TEXT NOT NULL,
+            	password TEXT NOT NULL,
             	status INTEGER CHECK (status in (0, 1)) DEFAULT 1
             );
         """.trimIndent())
