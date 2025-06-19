3749309cbbea3fb8b6b7ad642b66ff40c8fb0d11
Martin <martinc1998a@gmail.com>
2025-06-19 16:53:26 -0300

    adding success message to carnet printing

--- app/src/main/java/com/example/damproyectointegrador/MembershipCardActivity.kt
+++ app/src/main/java/com/example/damproyectointegrador/MembershipCardActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.Toast
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -39,7 +38,10 @@ class MembershipCardActivity : AppCompatActivity() {
         tvVencimiento.text = fechaVencimiento
 
         btnImprimir.setOnClickListener {
-            Toast.makeText(this, "Carnet impreso correctamente", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, ResultActivity::class.java).apply {
+                putExtra("message", "Carnet impreso correctamente!")
+            }
+            startActivity(intent)
         }
 
         bottomNavigationView.setOnItemSelectedListener { item ->
